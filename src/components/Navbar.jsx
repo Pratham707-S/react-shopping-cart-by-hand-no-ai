fd9dd9b255d15d579d55a512835a3c10706ed9a7
Prathamtiwari-web <Pratham.1226668@gmail.com>
2026-07-31 01:02:51 +0530

    Navbar ui-half/done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom'
+import { Home,ShoppingCart } from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <>
+  <header className="sticky top-0 z-50 bg-gray-950/95 backdrop-blur-md text-white shadow-2xl shadow-gray-950/70 border-b border-sky-600 p-4">
+  <div className="flex items-center justify-between">
+    
+    {/* Logo Section */}
+    <Link to={'/'} className="flex items-center space-x-3 cursor-pointer">
+      <Home className='w-8 h-8 text-sky-800 drop-shadow-lg'/>
+      <h1 className='text-3xl font-extrabold tracking-widest uppercase'>
+        KP <span className='text-sky-300'>Store</span>
+      </h1>
+    </Link>
+
+    {/* Navigation Section */}
+    <nav className='flex items-center space-x-6 mr-10'>
+      <Link to={'/cart'} className="hover:text-sky-300 transition-colors ">
+        <ShoppingCart className=''/>
+      </Link>
+      
+    </nav>
+    
+  </div>
+</header>
+   </>
   )
 }
 
